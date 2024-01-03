719368f5140b608f48e2f694b9c8fa314f03c0e2
Echeva26 <130578116+Echeva26@users.noreply.github.com>
2024-01-03 18:26:51 +0100

    Delete P4/P4/borrarP4.py

--- P4/P4/borrarP4.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-subprocess.run(["kubectl", "delete", "-f", "details/details.yaml"])
-subprocess.run(["kubectl", "delete", "-f", "ratings/ratings.yaml"])
-subprocess.run(["kubectl", "delete", "-f", "reviews/reviews-svc.yaml"])
-subprocess.run(["kubectl", "delete", "-f", "reviews/reviews-v1-deployment.yaml"])
-subprocess.run(["kubectl", "delete", "-f", "reviews/reviews-v2-deployment.yaml"])
-subprocess.run(["kubectl", "delete", "-f", "reviews/reviews-v3-deployment.yaml"])
-subprocess.run(["kubectl", "delete", "-f", "productpage/productpage.yaml"])
